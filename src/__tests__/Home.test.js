60bd81dd69d087f9ef9ea0978269050dfbb8d4ce
jayfr1122 <jayfranks21@gmail.com>
2024-02-07 15:53:45 -0800

    corrected mistakes in testing

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,10 +1,14 @@
-import { render } from "@testing-library/react"
-import Home from "../pages/Home"
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/Home";
+import { BrowserRouter } from 'react-router-dom';
 
 
 describe("<Home />", () => {
   it("renders without crashing", () => {
   const div = document.createElement("div")
   render(<Home />, div)
+  const homeImage = screen.getByRole("img")
+        expect(homeImage).toHaveAttribute("src", "home.png")
+        expect(homeImage).toHaveAttribute("alt", "Home")
 })
 })
