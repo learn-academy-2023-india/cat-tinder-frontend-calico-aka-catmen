d59f0c63bfc236b978e2eb496eb03a96f3ebc0cd
jayfr1122 <jayfranks21@gmail.com>
2024-02-07 16:00:02 -0800

    indentation correction

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,14 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../pages/Home";
-import { BrowserRouter } from 'react-router-dom';
-
 
 describe("<Home />", () => {
-  it("renders without crashing", () => {
-  const div = document.createElement("div")
-  render(<Home />, div)
-  const homeImage = screen.getByRole("img")
-        expect(homeImage).toHaveAttribute("src", "home.png")
-        expect(homeImage).toHaveAttribute("alt", "Home")
-})
+    it("renders without crashing", () => {
+        const div = document.createElement("div")
+    render(<Home />, div)
+        const homeImage = screen.getByRole("img")
+            expect(homeImage).toHaveAttribute("src", "home.png")
+            expect(homeImage).toHaveAttribute("alt", "Home")
+    })
 })
--- src/__tests__/Notfound.test.js
+++ src/__tests__/Notfound.test.js
@@ -3,10 +3,10 @@ import NotFound from "../pages/NotFound"
 
 describe("<NotFound />", () => {
     it("renders without crashing", () => {
-    const div = document.createElement("div")
+        const div = document.createElement("div")
     render(<NotFound />, div)
-    const notFoundImage = screen.getByRole("img")
-        expect(notFoundImage).toHaveAttribute("src", "404.png")
-        expect(notFoundImage).toHaveAttribute("alt", "404 Not Found")
-  })
-  })
+        const notFoundImage = screen.getByRole("img")
+            expect(notFoundImage).toHaveAttribute("src", "404.png")
+            expect(notFoundImage).toHaveAttribute("alt", "404 Not Found")
+    })
+})
